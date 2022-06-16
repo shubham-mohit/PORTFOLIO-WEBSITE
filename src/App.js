d63f1fcdb899f9d44b74b840ee3700514fd76557
Rajesh270712 <rajeshbagul2001@gmail.com>
2022-06-17 03:10:29 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 
 import Navbar from "./components/Navbar";
 import Loader from "./components/Loader";
+import Home from "./components/Home"
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -18,6 +19,7 @@ function App() {
     ) : (
     <div className="App text-gray-400 bg-gray-900 body-font">
 <Navbar />
+<Home/>
     </div>
   );
 }
